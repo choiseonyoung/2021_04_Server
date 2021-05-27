4e63dc3078d166365fecd38bb9739cdb7051029f
choiseonyoung <bviibb@naver.com>
2021-05-27 16:29:02 +0900

    TO DO LIST_servlet

---
+++ Server_013_ToList/src/main/java/com/callor/todo/command/HomeCommandImplV1.java
@@ -0,0 +1,16 @@
+package com.callor.todo.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeCommandImplV1 implements TodoCommand {
+
+	@Override
+	public void excute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		ReqCommand.forward(req,res,"home");
+	}
+
+}
